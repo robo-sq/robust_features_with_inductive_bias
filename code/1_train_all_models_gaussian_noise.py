2bbfba8193134f87dcf2badead3141e062f7b12d
robo-sq <31593127+robo-sq@users.noreply.github.com>
2019-07-01 19:20:05 +0100

    try to mount google drive

--- code/1_train_all_models_gaussian_noise.py
+++ code/1_train_all_models_gaussian_noise.py
@@ -16,7 +16,8 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 all_models = ['cnn_25_noreg', 'cnn_deep_noreg'] 
 
 # save path
-results_path = '../results'
+results_path = '/content/gdrive/My Drive/results'
+
 model_path = utils.make_directory(results_path, 'model_params')
 
 
